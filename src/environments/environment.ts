fc5bba316d398b34ea8788c556d0f30c822b8bb9
Periapandi Jeyaram <jperiapandi@gmail.com>
2025-07-14 23:07:09 +0530

    Angualr Fire added

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'cricket-team-creator',
+    appId: '1:192315382750:web:ffd0b103c6ae087e615b4f',
+    storageBucket: 'cricket-team-creator.firebasestorage.app',
+    apiKey: 'AIzaSyCr7nK8cDWNe4S6otg8aZCnuGw2Zk2E6ao',
+    authDomain: 'cricket-team-creator.firebaseapp.com',
+    messagingSenderId: '192315382750',
+    measurementId: 'G-773QG03MM7',
+  },
+};

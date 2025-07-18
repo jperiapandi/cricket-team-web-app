29144693670531024592e73544f25b93ecc2dd57
Periapandi Jeyaram <jperiapandi@gmail.com>
2025-07-18 16:53:07 +0530

    Updated ui display

--- src/app/components/available-player/available-player.component.ts
+++ src/app/components/available-player/available-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { Player } from '../../types/player';
 
 @Component({
@@ -6,13 +6,27 @@ import { Player } from '../../types/player';
   imports: [],
   templateUrl: './available-player.component.html',
   styles: ``,
-  host:{
-    'class':'available-player-cmp'
-  }
+  host: {
+    class: 'available-player-cmp',
+  },
 })
 export class AvailablePlayerComponent {
   player = input.required<Player>();
+  readonly roleIcon = computed(() => {
+    switch (this.player().role) {
+      case 'Batter':
+        return 'bat.svg';
 
+      case 'Bowler':
+        return 'ball.svg';
+
+      case 'Wicket-Keeper':
+        return 'plyr-keeper.svg';
+
+      default:
+        return 'all-rounder.svg';
+    }
+  });
   select = output<number>();
 
   onSelectClick(evt: MouseEvent, id: number) {
